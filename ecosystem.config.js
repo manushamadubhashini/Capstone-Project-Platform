4604e4e1bcb2739d0d9becbe218ca680cf6c5dba
Acer <madubhashinimanusha@gmail.com>
2026-03-26 22:57:14 +0530

    updated ecosystem.config.js file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,19 +1,22 @@
 module.exports = {
     apps: [
         {
-            name: "Config-Server",
-            args: "java -jar ./Config-Server/target/Config-Server-1.0.0.jar",
+            name: "config-server",
+            script: "java",
+            args: "-jar ./Config-Server/target/Config-Server-1.0.0.jar",
             log_file: "./logs/Config-Server.log"
         },
         {
-            name: "Service-Registry",
-            args: "java -jar ./Service-Registry/target/Service-Registry-1.0.0.jar",
+            name: "service-registry",
+            script: "java",
+            args: "-jar ./Service-Registry/target/Service-Registry-1.0.0.jar",
             log_file: "./logs/Service-Registry.log"
         },
         {
-            name: "Api-Gateway",
-            args: "java -jar ./Api-Gateway/target/Api-Gateway-1.0.0.jar",
-            log_file: "./logs/Api-Gateway/logs/.log"
+            name: "api-gateway",
+            script: "java",
+            args: "-jar ./Api-Gateway/target/Api-Gateway-1.0.0.jar",
+            log_file: "./logs/Api-Gateway.log"
         }
     ]
-}
+};
